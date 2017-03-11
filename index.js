ecd4fdd52303eb4a895f0c6035b1939843b59d87
sedlukha <rutrovich@gmail.com>
2017-03-11 23:53:33 +0300

    oprimization

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = postcss.plugin('postcss-interpolate', () => {
         }
 
         function isOdd(array) {
-            return (array.length & 1)
+            return (array.length & 1);
         }
 
         function isPx(i) {
@@ -37,99 +37,6 @@ module.exports = postcss.plugin('postcss-interpolate', () => {
 
         function isRem(i) {
             return i.indexOf('rem') > -1;
-        }
-
-        // Generate media and values arrays
-        function generateArrays(interpolateArray, decl) {
-            const rule = decl.parent;
-            const root = rule.parent;
-            var mediaArray = [];
-            var valueArray = [];
-
-            if (!isOdd(interpolateArray) && !isString(interpolateArray[0])) {
-                directionViewport = 'vw';
-                startFrom = 0;
-            } else if (isOdd(interpolateArray) && isString(interpolateArray[0])) {
-                var direction = interpolateArray[0];
-
-                if ((direction.indexOf('horizontally') > -1) || (direction.indexOf('vw') > -1)) {
-                    directionViewport = 'vw';
-                } else if ((direction.indexOf('vertically') > -1) || (direction.indexOf('vh') > -1)) {
-                    directionViewport = 'vh';
-                } else {
-                    throw decl.error('Deprecated direction property', {
-                        word: interpolateArray[0]
-                    });
-                }
-                startFrom = 1;
-            } else {
-                // Syntax error
-                throw decl.error('Deprecated value syntax', {
-                    word: decl.value
-                });
-            }
-
-            for (var i = startFrom; i < interpolateArray.length; i += 2) {
-                mediaArray.push(interpolateArray[i]);
-                valueArray.push(interpolateArray[i + 1]);
-            }
-
-            // First @media rule
-            decl.replaceWith({
-                prop: decl.prop,
-                value: interpolateArray[startFrom + 1]
-            });
-
-            // Middle @media rule
-            for (var i = 0; i < mediaArray.length - 1; i++) {
-                for (var i = 0; i < valueArray.length - 1; i++) {
-
-                    // Generate @media atrule
-                    var mediaAtrule = postcss.atRule({
-                        name: 'media',
-                        params: 'screen and (min-width: ' + mediaArray[i] + ')'
-                    })
-
-                    var maxMedia = mediaArray[i + 1];
-                    var minMedia = mediaArray[i];
-                    var maxValue = valueArray[i + 1];
-                    var minValue = valueArray[i];
-
-                    // Append current selector with properties and values
-                    mediaAtrule.append({
-                        selector: rule.selector
-                    }).walkRules( function (selector) {
-                        selector.append({
-                            prop: decl.prop,
-                            value: 'calc(' + minValue + ' + ' + (parseFloat(maxValue) - parseFloat(minValue)) + ' * (100' + directionViewport + ' - ' + minMedia + ') / ' + ((parseFloat(maxMedia) - parseFloat(minMedia)) / getHtmlValue(maxMedia, mediaArray, valueArray, decl)) + ')'
-                        });
-                    });
-
-                    // Insert middle mediaquery
-                    root.insertAfter(root.last, mediaAtrule);
-                }
-            }
-
-            // Last @media rule
-            var lastMedia = postcss.atRule({
-                name: 'media',
-                params: 'screen and (min-width: ' + mediaArray[mediaArray.length - 1] + ')'
-            });
-
-            // Append current selector with properties and values
-            lastMedia.append({
-                selector: rule.selector
-            }).walkRules( function (selector) {
-                selector.append({
-                    prop: decl.prop,
-                    value: valueArray[valueArray.length - 1]
-                });
-            });
-
-            // Insert last media rule
-            root.insertAfter(root.last, lastMedia);
-
-            return;
         }
 
         // Get html font-size value
@@ -198,6 +105,99 @@ module.exports = postcss.plugin('postcss-interpolate', () => {
             }
         }
 
+        // Generate media and values arrays
+        function interpolate(interpolateArray, decl) {
+            const rule = decl.parent;
+            const root = rule.parent;
+            var mediaArray = [];
+            var valueArray = [];
+
+            if (!isOdd(interpolateArray) && !isString(interpolateArray[0])) {
+                directionViewport = 'vw';
+                startFrom = 0;
+            } else if (isOdd(interpolateArray) && isString(interpolateArray[0])) {
+                var direction = interpolateArray[0];
+
+                if ((direction.indexOf('horizontally') > -1) || (direction.indexOf('vw') > -1)) {
+                    directionViewport = 'vw';
+                } else if ((direction.indexOf('vertically') > -1) || (direction.indexOf('vh') > -1)) {
+                    directionViewport = 'vh';
+                } else {
+                    throw decl.error('Deprecated direction property', {
+                        word: interpolateArray[0]
+                    });
+                }
+                startFrom = 1;
+            } else {
+                // Syntax error
+                throw decl.error('Deprecated value syntax', {
+                    word: decl.value
+                });
+            }
+
+            for (var i = startFrom; i < interpolateArray.length; i += 2) {
+                mediaArray.push(interpolateArray[i]);
+                valueArray.push(interpolateArray[i + 1]);
+            }
+
+            // First @media rule
+            decl.replaceWith({
+                prop: decl.prop,
+                value: interpolateArray[startFrom + 1]
+            });
+
+            // Middle @media rule
+            for (var i = 0; i < mediaArray.length - 1; i++) {
+                for (var i = 0; i < valueArray.length - 1; i++) {
+
+                    // Generate @media atrule
+                    var mediaAtrule = postcss.atRule({
+                        name: 'media',
+                        params: 'screen and (min-width: ' + mediaArray[i] + ')'
+                    });
+
+                    var maxMedia = mediaArray[i + 1];
+                    var minMedia = mediaArray[i];
+                    var maxValue = valueArray[i + 1];
+                    var minValue = valueArray[i];
+
+                    // Append current selector with properties and values
+                    mediaAtrule.append({
+                        selector: rule.selector
+                    }).walkRules( function (selector) {
+                        selector.append({
+                            prop: decl.prop,
+                            value: 'calc(' + minValue + ' + ' + (parseFloat(maxValue) - parseFloat(minValue)) + ' * (100' + directionViewport + ' - ' + minMedia + ') / ' + ((parseFloat(maxMedia) - parseFloat(minMedia)) / getHtmlValue(maxMedia, mediaArray, valueArray, decl)) + ')'
+                        });
+                    });
+
+                    // Insert middle mediaquery
+                    root.insertAfter(root.last, mediaAtrule);
+                }
+            }
+
+            // Last @media rule
+            var lastMedia = postcss.atRule({
+                name: 'media',
+                params: 'screen and (min-width: ' + mediaArray[mediaArray.length - 1] + ')'
+            });
+
+            // Append current selector with properties and values
+            lastMedia.append({
+                selector: rule.selector
+            }).walkRules( function (selector) {
+                selector.append({
+                    prop: decl.prop,
+                    value: valueArray[valueArray.length - 1]
+                });
+            });
+
+            // Insert last media rule
+            root.insertAfter(root.last, lastMedia);
+
+            return;
+        }
+
         css.walkDecls(decl => {
 
             // Get property line
@@ -210,7 +210,7 @@ module.exports = postcss.plugin('postcss-interpolate', () => {
                 if (interpolateArray.length > 3) {
 
                     // Get array of mediaqueries and array of values
-                    generateArrays(interpolateArray,  decl);
+                    interpolate(interpolateArray,  decl);
 
                 } else {
                     throw decl.error('Deprecated property amount', {
